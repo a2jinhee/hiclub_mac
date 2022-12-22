409dbd0b8dc6951d87c801e3b324b58536f6d403
Jinhee Kim <a2jinhee@gmail.com>
2022-12-22 14:30:33 +0900

    clean-up 12.22.22

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User, Post, Category, Author
 from django.db.models import Q
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -75,3 +77,62 @@ def search(request):
         'queryset': queryset
     }
     return render(request, 'search_bar.html', context)
+
+
+def Register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match.")
+            return redirect('/register')
+
+        user = User.objects.create_user(username, email, password1)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        return render(request, 'login.html')
+    return render(request, "register.html")
+
+
+def Login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid Credentials")
+        return render(request, 'blog.html')
+    return render(request, "login.html")
+
+
+def Profile(request):
+    return render(request, "profile.html")
+
+
+def edit_profile(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method == "POST":
+        form = ProfileForm(data=request.POST,
+                           files=request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            alert = True
+            return render(request, "edit_profile.html", {'alert': alert})
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, "edit_profile.html", {'form': form})
